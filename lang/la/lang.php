ffe8ce1e3e34312f7b4cad19eba27138845deb0e
Beta134 <guerraart14@gmail.com>
2024-06-30 13:55:40 +0200

    translation update

---
+++ lang/la/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Beta134 <guerraart14@gmail.com>
+ */
+$lang['anss_input_btn_save']   = 'Web';
+$lang['anss_input_btn_save_descr'] = 'Ww2
+';
